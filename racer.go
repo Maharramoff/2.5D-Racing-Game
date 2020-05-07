e5afe02c089f1f968a0e393b87a601bdab476152
Maharramoff <sh.maharramov@gmail.com>
2020-05-07 18:58:49 +0400

    update: switch to sfml

--- racer.go
+++ racer.go
@@ -1,64 +1,39 @@
 package main
 
 import (
-	"fmt"
-	"github.com/veandco/go-sdl2/sdl"
+	"gopkg.in/teh-cmc/go-sfml.v24/graphics"
+	"gopkg.in/teh-cmc/go-sfml.v24/window"
+	"runtime"
 )
 
-const (
-	screenWidth  = 600
-	screenHeight = 600
-)
-
-func draw(renderer *sdl.Renderer) {
-	renderer.Clear()
-	renderer.SetDrawColor(255, 255, 255, 255)
-	renderer.Present()
-}
-
-func eventHandler(quit *bool) {
-	for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-		switch eType := event.(type) {
-		case *sdl.QuitEvent:
-			*quit = true
-		case *sdl.KeyboardEvent:
-			if eType.Keysym.Sym == sdl.K_ESCAPE {
-				*quit = true
-			}
-		}
-	}
-}
+func init() { runtime.LockOSThread() }
 
 func main() {
-	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-		fmt.Println("Init SDL: ", err)
-		return
-	}
+	videoMode := window.NewSfVideoMode()
+	defer window.DeleteSfVideoMode(videoMode)
+	videoMode.SetWidth(1024)
+	videoMode.SetHeight(768)
+	videoMode.SetBitsPerPixel(32)
 
-	window, err := sdl.CreateWindow(
-		"2.5D Racing Demo",
-		sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		screenWidth, screenHeight,
-		sdl.WINDOW_OPENGL)
-	if err != nil {
-		fmt.Println("Init Window: ", err)
-		return
-	}
+	/* Create the main window */
+	contextSettings := window.NewSfContextSettings()
+	defer window.DeleteSfContextSettings(contextSettings)
+	app := graphics.SfRenderWindow_create(videoMode, "SFML window", uint(window.SfResize|window.SfClose), contextSettings)
+	defer window.SfWindow_destroy(app)
 
-	defer window.Destroy()
+	ev := window.NewSfEvent()
+	defer window.DeleteSfEvent(ev)
 
-	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
-	if err != nil {
-		fmt.Println("Init Renderer: ", err)
-		return
-	}
-
-	defer renderer.Destroy()
-
-	var quit = false
-
-	for !quit {
-		eventHandler(&quit)
-		draw(renderer)
+	/* Start the game loop */
+	for window.SfWindow_isOpen(app) > 0 {
+		/* Process events */
+		for window.SfWindow_pollEvent(app, ev) > 0 {
+			/* Close window: exit */
+			if ev.GetXtype() == window.SfEventType(window.SfEvtClosed) {
+				return
+			}
+		}
+		graphics.SfRenderWindow_clear(app, graphics.GetSfRed())
+		graphics.SfRenderWindow_display(app)
 	}
 }
